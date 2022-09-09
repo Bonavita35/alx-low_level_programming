880c03e724c69537bc4f93695939d151fcab85f1
Bonavita35 <okwyno12@gmail.com>
2022-09-08 17:36:29 -0700

    code with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-
-
+/**
+ * main - print the string in the put function
+ *prints this output: Programming is like building a multilingual puzzle
+ *return 0
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return 0;
-
-}
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
 
 
