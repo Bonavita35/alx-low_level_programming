e992fa4800dd1526b242576dbd824a8bb169f156
Bonavita35 <okwyno12@gmail.com>
2022-09-12 11:51:48 -0700

    code with c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - print a-z to std output
+ * Return: Exit status is zero if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	} putchar('\n');
+	return (0);
+}
