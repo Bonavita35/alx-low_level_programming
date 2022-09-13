10980c0429d34a707c6f3bb25f7103c29a802eb0
Bonavita35 <okwyno12@gmail.com>
2022-09-13 16:34:20 -0700

    code with c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar on the screen
+ * Return: On success, 0.
+ */
+int main(void)
+{
+	char var[] = "_putchar";
+	int i;
+	int lenVar = sizeof(var) / sizeof(var[0]);
+
+	for (i = 0; i < lenVar - 1; i++)
+	{
+		_putchar(var[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints a-z in lowercase
+ * Return: returns nothing/void
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++
+	}
+
+	_putchar('\n');
+}
