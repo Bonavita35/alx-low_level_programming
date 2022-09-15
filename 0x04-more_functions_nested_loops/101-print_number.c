8135675fe0151341da6876713d95792c462202d1
Bonavita35 <okwyno12@gmail.com>
2022-09-15 09:43:10 -0700

    code with c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/* print declaration */
+
+void print(unsigned int d);
+
+/**
+ * print_number - Prints an integer
+ * @n: input integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	print(n);
+}
+
+/**
+ * print - subfunction to print recursively
+ * @d: variable to be printed
+ */
+void print(unsigned int d)
+{
+	if (d / 10)
+		print(d / 10);
+	_putchar('0'  + d % 10);
+}
