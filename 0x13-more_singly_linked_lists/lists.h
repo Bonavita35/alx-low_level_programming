e66e179a30d384ce6b7b3277e4fd25fdc32b20cd
bonavita35 <okwyno12@gmail.com>
2022-10-26 04:11:34 -0700

    c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,15 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  * Description: singly linked list node structure
+ * for Holberton project
  */
 
 typedef struct listint_s
@@ -17,7 +17,22 @@ typedef struct listint_s
 	int n;
 
 	struct listint_s *next;
+
 } listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ * Description: singly linked list of pointers
+ */
+
+typedef struct listp_s
+{
+	void *p;
+
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
